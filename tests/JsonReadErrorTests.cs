61f9af3407323190175a1d516ab126445ec2760b
Atif Aziz <code@raboof.com>
2022-02-03 11:07:43 +0100

    Add tests for result types

---
+++ tests/JsonReadErrorTests.cs
@@ -0,0 +1,26 @@
+// Copyright (c) 2021 Atif Aziz.
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace JsonR.Tests;
+
+using Xunit;
+
+public class JsonReadErrorTests
+{
+    [Fact]
+    public void Constructor_Initializes_Message()
+    {
+        var message = "oops";
+        var error = new JsonReadError(message);
+        Assert.Equal(message, error.Message);
+    }
+
+    [Fact]
+    public void ToString_Returns_Message()
+    {
+        var message = "oops";
+        var error = new JsonReadError(message);
+        Assert.Equal(message, error.ToString());
+    }
+}
---
+++ tests/JsonReadResultTests.cs
@@ -0,0 +1,44 @@
+// Copyright (c) 2021 Atif Aziz.
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace JsonR.Tests;
+
+using Xunit;
+
+public class JsonReadResultTests
+{
+    [Fact]
+    public void Value_Represents_Value_Result()
+    {
+        var (value, error) = JsonReadResult.Value(42);
+        Assert.Null(error);
+        Assert.Equal(42, value);
+    }
+
+    [Fact]
+    public void Converts_JsonReadError_To_Error_Result()
+    {
+        var error = new JsonReadError("oops");
+        JsonReadResult<int> result = error;
+        Assert.NotNull(result.Error);
+        Assert.Same(error.Message, result.Error);
+        Assert.Equal(0, result.Value);
+    }
+
+    [Fact]
+    public void ToString_With_Value_Result_Returns_Value_Prefixed_String()
+    {
+        var str = JsonReadResult.Value(42).ToString();
+        Assert.Equal("Value: 42", str);
+    }
+
+    [Fact]
+    public void ToString_With_Error_Result_Returns_Error_Prefixed_String()
+    {
+        var error = new JsonReadError("oops");
+        JsonReadResult<int> result = error;
+        var str = result.ToString();
+        Assert.Equal("Error: oops", str);
+    }
+}
