2a9919780357f1c72317896e471d0b2f4150c7a4
Bastian Burger <22341213+bastbu@users.noreply.github.com>
2022-11-25 15:44:44 +0100

    Update Github API benchmark with entire JSON response snippet

--- bench/GithubApiBenchmark.cs
+++ bench/GithubApiBenchmark.cs
@@ -3,7 +3,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -18,18 +17,103 @@ using BenchmarkDotNet.Attributes;
 [MemoryDiagnoser]
 public class GithubApiBenchmark
 {
-    static readonly MergeBranchResponse TestData =
-        new(new Uri("https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e"),
-            "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-            new Commit(new Uri("https://api.github.com/repos/octocat/Hello-World/git/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e"),
-                       new Author("Monalisa Octocat", "mona@github.com",
-                                  DateTime.Parse("2011-04-14T16:00:49Z",
-                                                 CultureInfo.InvariantCulture)),
-                       new Author("Monalisa Octocat", "mona@github.com",
-                                  DateTime.Parse("2011-04-14T16:00:49Z",
-                                                 CultureInfo.InvariantCulture)),
-                       "fix all the bugs",
-                       12));
+    const string TestData = /*lang=json,strict*/ """
+        {
+          "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+          "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+          "node_id": "MDY6Q29tbWl0NmRjYjA5YjViNTc4NzVmMzM0ZjYxYWViZWQ2OTVlMmU0MTkzZGI1ZQ==",
+          "html_url": "https://github.com/octocat/Hello-World/commit/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+          "comments_url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e/comments",
+          "commit": {
+            "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+            "author": {
+              "name": "Monalisa Octocat",
+              "email": "mona@github.com",
+              "date": "2011-04-14T16:00:49Z"
+            },
+            "committer": {
+              "name": "Monalisa Octocat",
+              "email": "mona@github.com",
+              "date": "2011-04-14T16:00:49Z"
+            },
+            "message": "Fix all the bugs",
+            "tree": {
+              "url": "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+              "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+            },
+            "comment_count": 0,
+            "verification": {
+              "verified": false,
+              "reason": "unsigned",
+              "signature": null,
+              "payload": null
+            }
+          },
+          "author": {
+            "login": "octocat",
+            "id": 1,
+            "node_id": "MDQ6VXNlcjE=",
+            "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/octocat",
+            "html_url": "https://github.com/octocat",
+            "followers_url": "https://api.github.com/users/octocat/followers",
+            "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+            "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+            "organizations_url": "https://api.github.com/users/octocat/orgs",
+            "repos_url": "https://api.github.com/users/octocat/repos",
+            "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/octocat/received_events",
+            "type": "User",
+            "site_admin": false
+          },
+          "committer": {
+            "login": "octocat",
+            "id": 1,
+            "node_id": "MDQ6VXNlcjE=",
+            "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/octocat",
+            "html_url": "https://github.com/octocat",
+            "followers_url": "https://api.github.com/users/octocat/followers",
+            "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+            "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+            "organizations_url": "https://api.github.com/users/octocat/orgs",
+            "repos_url": "https://api.github.com/users/octocat/repos",
+            "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/octocat/received_events",
+            "type": "User",
+            "site_admin": false
+          },
+          "parents": [
+            {
+              "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+              "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+            }
+          ],
+          "stats": {
+            "additions": 104,
+            "deletions": 4,
+            "total": 108
+          },
+          "files": [
+            {
+              "filename": "file1.txt",
+              "additions": 10,
+              "deletions": 2,
+              "changes": 12,
+              "status": "modified",
+              "raw_url": "https://github.com/octocat/Hello-World/raw/7ca483543807a51b6079e54ac4cc392bc29ae284/file1.txt",
+              "blob_url": "https://github.com/octocat/Hello-World/blob/7ca483543807a51b6079e54ac4cc392bc29ae284/file1.txt",
+              "patch": "@@ -29,7 +29,7 @@\n....."
+            }
+          ]
+        }
+        """;
 
     private static readonly IJsonReader<Author> AuthorJsonReader =
         JsonReader.Object(JsonReader.Property("name", JsonReader.String()),
@@ -59,7 +143,7 @@ public class GithubApiBenchmark
     [GlobalSetup]
     public void Setup()
     {
-        var json = JsonSerializer.Serialize(Enumerable.Repeat(TestData, ObjectCount));
+        var json = $"[{string.Join(",", Enumerable.Repeat(TestData, ObjectCount))}]";
         this.jsonDataBytes = Encoding.UTF8.GetBytes(json);
     }
 
