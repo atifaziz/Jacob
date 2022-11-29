8869c17aa836bc1c71dfe889c15882adc57fe9dd
Bastian Burger <22341213+bastbu@users.noreply.github.com>
2022-11-29 17:29:33 +0100

    Use complete response from GitHub API benchmark (#15)

--- bench/GithubApiBenchmark.cs
+++ bench/GithubApiBenchmark.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -115,26 +116,102 @@ public class GithubApiBenchmark
         }
         """;
 
-    private static readonly IJsonReader<Author> AuthorJsonReader =
+    static readonly IJsonReader<Uri> UriReader =
+        from s in JsonReader.String()
+        select new Uri(s);
+
+    static readonly IJsonReader<CommitUser> CommitUserJsonReader =
         JsonReader.Object(JsonReader.Property("name", JsonReader.String()),
                           JsonReader.Property("email", JsonReader.String()),
                           JsonReader.Property("date", JsonReader.DateTime()),
-                          (name, email, date) => new Author(name, email, date));
+                          (p1, p2, p3) => new CommitUser(p1, p2, p3));
 
-    private static readonly IJsonReader<Commit> CommitJsonReader =
-        JsonReader.Object(JsonReader.Property("url", from s in JsonReader.String() select new Uri(s)),
-                          JsonReader.Property("author", AuthorJsonReader),
-                          JsonReader.Property("committer", AuthorJsonReader),
+    static readonly IJsonReader<Tree> TreeJsonReader =
+        JsonReader.Object(JsonReader.Property("url", UriReader),
+                          JsonReader.Property("sha", JsonReader.String()),
+                          (p1, p2) => new Tree(p1, p2));
+
+    static readonly IJsonReader<Verification> VerificationJsonReader =
+        JsonReader.Object(JsonReader.Property("verified", JsonReader.Boolean()),
+                          JsonReader.Property("reason", JsonReader.String()),
+                          JsonReader.Property("signature", Nullable(JsonReader.String(), null)),
+                          JsonReader.Property("payload", Nullable(JsonReader.String(), null)),
+                          (p1, p2, p3, p4) => new Verification(p1, p2, p3, p4));
+
+    static readonly IJsonReader<Commit> CommitJsonReader =
+        JsonReader.Object(JsonReader.Property("url", UriReader),
+                          JsonReader.Property("author", CommitUserJsonReader),
+                          JsonReader.Property("committer", CommitUserJsonReader),
                           JsonReader.Property("message", JsonReader.String()),
+                          JsonReader.Property("tree", TreeJsonReader),
                           JsonReader.Property("comment_count", JsonReader.Int32()),
-                          (url, author, committer, message, commentCount) =>
-                              new Commit(url, author, committer, message, commentCount));
+                          JsonReader.Property("verification", VerificationJsonReader),
+                          (p1, p2, p3, p4, p5, p6, p7) =>
+                              new Commit(p1, p2, p3, p4, p5, p6, p7));
+
+    static readonly IJsonReader<GitHubUser> GitHubUserJsonReader =
+        JsonReader.Object(JsonReader.Property("login", JsonReader.String()),
+                          JsonReader.Property("id", JsonReader.Int32()),
+                          JsonReader.Property("node_id", JsonReader.String()),
+                          JsonReader.Property("avatar_url", UriReader),
+                          JsonReader.Property("gravatar_id", JsonReader.String()),
+                          JsonReader.Property("url", UriReader),
+                          JsonReader.Property("html_url", UriReader),
+                          JsonReader.Property("followers_url", UriReader),
+                          JsonReader.Property("following_url", UriReader),
+                          JsonReader.Property("gists_url", UriReader),
+                          JsonReader.Property("starred_url", UriReader),
+                          JsonReader.Property("subscriptions_url", UriReader),
+                          JsonReader.Property("organizations_url", UriReader),
+                          JsonReader.Property("repos_url", UriReader),
+                          JsonReader.Property("events_url", UriReader),
+                          JsonReader.Property("received_events_url", UriReader),
+                          /*
+                           * Omitted because JsonReader.Object supports 16 properties.
+                           * JsonReader.Property("type", JsonReader.String()),
+                           * JsonReader.Property("site_admin", JsonReader.Boolean()
+                           */
+                          (p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16) =>
+                              new GitHubUser(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16));
+
+    static readonly IJsonReader<Stats> StatsJsonReader =
+        JsonReader.Object(JsonReader.Property("additions", JsonReader.Int32()),
+                          JsonReader.Property("deletions", JsonReader.Int32()),
+                          JsonReader.Property("total", JsonReader.Int32()),
+                          (p1, p2, p3) => new Stats(p1, p2, p3));
+
+    static readonly IJsonReader<File> FileJsonReader =
+        JsonReader.Object(JsonReader.Property("filename", JsonReader.String()),
+                          JsonReader.Property("additions", JsonReader.Int32()),
+                          JsonReader.Property("deletions", JsonReader.Int32()),
+                          JsonReader.Property("changes", JsonReader.Int32()),
+                          JsonReader.Property("status", JsonReader.String()),
+                          JsonReader.Property("raw_url", UriReader),
+                          JsonReader.Property("blob_url", UriReader),
+                          JsonReader.Property("patch", JsonReader.String()),
+                          (p1, p2, p3, p4, p5, p6, p7, p8) =>
+                              new File(p1, p2, p3, p4, p5, p6, p7, p8));
 
     static readonly IJsonReader<MergeBranchResponse> MergeBranchResponseJsonReader =
-        JsonReader.Object(JsonReader.Property("url", from s in JsonReader.String() select new Uri(s)),
+        JsonReader.Object(JsonReader.Property("url", UriReader),
                           JsonReader.Property("sha", JsonReader.String()),
+                          JsonReader.Property("node_id", JsonReader.String()),
+                          JsonReader.Property("html_url", UriReader),
+                          JsonReader.Property("comments_url", UriReader),
                           JsonReader.Property("commit", CommitJsonReader),
-                          (url, sha, commit) => new MergeBranchResponse(url, sha, commit));
+                          JsonReader.Property("author", GitHubUserJsonReader),
+                          JsonReader.Property("committer", GitHubUserJsonReader),
+                          JsonReader.Property("parents", ImmutableArrayReader(TreeJsonReader)),
+                          JsonReader.Property("stats", StatsJsonReader),
+                          JsonReader.Property("files", ImmutableArrayReader(FileJsonReader)),
+                          (p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11) =>
+                              new MergeBranchResponse(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
+
+    static IJsonReader<T?> Nullable<T>(IJsonReader<T> reader, T? @null) =>
+        JsonReader.Either(JsonReader.Null(@null), from v in reader select (T?)v);
+
+    static IJsonReader<ImmutableArray<T>> ImmutableArrayReader<T>(IJsonReader<T> reader) =>
+        JsonReader.Array(reader, list => list.ToImmutableArray());
 
     [Params(10, 100, 1000, 10000)] public int ObjectCount { get; set; }
 
@@ -158,14 +235,67 @@ public class GithubApiBenchmark
 
 public sealed record MergeBranchResponse([property: JsonPropertyName("url")] Uri Url,
                                          [property: JsonPropertyName("sha")] string Sha,
-                                         [property: JsonPropertyName("commit")] Commit Commit);
+                                         [property: JsonPropertyName("node_id")] string NodeId,
+                                         [property: JsonPropertyName("html_url")] Uri HtmlUrl,
+                                         [property: JsonPropertyName("comments_url")] Uri CommentsUrl,
+                                         [property: JsonPropertyName("commit")] Commit Commit,
+                                         [property: JsonPropertyName("author")] GitHubUser Author,
+                                         [property: JsonPropertyName("committer")] GitHubUser Committer,
+                                         [property: JsonPropertyName("parents")] ImmutableArray<Tree> Parents,
+                                         [property: JsonPropertyName("stats")] Stats Stats,
+                                         [property: JsonPropertyName("files")] ImmutableArray<File> Files);
 
 public sealed record Commit([property: JsonPropertyName("url")] Uri Url,
-                            [property: JsonPropertyName("author")] Author Author,
-                            [property: JsonPropertyName("committer")] Author Committer,
+                            [property: JsonPropertyName("author")] CommitUser Author,
+                            [property: JsonPropertyName("committer")] CommitUser Committer,
                             [property: JsonPropertyName("message")] string Message,
-                            [property: JsonPropertyName("comment_count")] int CommentCount);
+                            [property: JsonPropertyName("tree")] Tree Tree,
+                            [property: JsonPropertyName("comment_count")] int CommentCount,
+                            [property: JsonPropertyName("verification")] Verification Verification);
+    
+public sealed record CommitUser([property: JsonPropertyName("name")] string Name,
+                                [property: JsonPropertyName("email")] string Email,
+                                [property: JsonPropertyName("date")] DateTime Date);
 
-public sealed record Author([property: JsonPropertyName("name")] string Name,
-                            [property: JsonPropertyName("email")] string Email,
-                            [property: JsonPropertyName("date")] DateTime Date);
+public sealed record Tree([property: JsonPropertyName("url")] Uri Url,
+                          [property: JsonPropertyName("sha")] string Sha);
+
+public sealed record Verification([property: JsonPropertyName("verified")] bool Verified,
+                                  [property: JsonPropertyName("reason")] string Reason,
+                                  [property: JsonPropertyName("signature")] string? Signature,
+                                  [property: JsonPropertyName("payload")] string? Payload);
+
+public sealed record GitHubUser([property: JsonPropertyName("login")] string Login,
+                                [property: JsonPropertyName("id")] int Id,
+                                [property: JsonPropertyName("node_id")] string NodeId,
+                                [property: JsonPropertyName("avatar_url")] Uri AvatarUrl,
+                                [property: JsonPropertyName("gravatar_id")] string GravatarId,
+                                [property: JsonPropertyName("url")] Uri Url,
+                                [property: JsonPropertyName("html_url")] Uri HtmlUrl,
+                                [property: JsonPropertyName("followers_url")] Uri FollowersUrl,
+                                [property: JsonPropertyName("following_url")] Uri FollowingUrl,
+                                [property: JsonPropertyName("gists_url")] Uri GistsUrl,
+                                [property: JsonPropertyName("starred_url")] Uri StarredUrl,
+                                [property: JsonPropertyName("subscriptions_url")] Uri SubscriptionsUrl,
+                                [property: JsonPropertyName("organizations_url")] Uri OrganizationsUrl,
+                                [property: JsonPropertyName("repos_url")] Uri ReposUrl,
+                                [property: JsonPropertyName("events_url")] Uri EventsUrl,
+                                [property: JsonPropertyName("received_events_url")] Uri ReceivedEventsUrl
+                                /*
+                                 * Omitted because JsonReader.Object supports 16 properties.
+                                 * [property: JsonPropertyName("type")] string Type,
+                                 * [property: JsonPropertyName("site_admin")] bool SiteAdmin
+                                 */);
+
+public sealed record Stats([property: JsonPropertyName("additions")] int Additions,
+                           [property: JsonPropertyName("deletions")] int Deletions,
+                           [property: JsonPropertyName("total")] int Total);
+
+public sealed record File([property: JsonPropertyName("filename")] string Filename,
+                          [property: JsonPropertyName("additions")] int Additions,
+                          [property: JsonPropertyName("deletions")] int Deletions,
+                          [property: JsonPropertyName("changes")] int Changes,
+                          [property: JsonPropertyName("status")] string Status,
+                          [property: JsonPropertyName("raw_url")] Uri RawUrl,
+                          [property: JsonPropertyName("blob_url")] Uri BlobUrl,
+                          [property: JsonPropertyName("patch")] string Patch);
